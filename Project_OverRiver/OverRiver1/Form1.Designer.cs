caa0de047c4471af3fb4fe5495a5718e57ab2b0b
Nana <37291738+h93279@users.noreply.github.com>
2018-05-29 12:52:54 +0800

    Add files via upload

---
+++ Project_OverRiver/OverRiver1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace OverRiver1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 36;
+            this.listBox1.Location = new System.Drawing.Point(133, 270);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 292);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 340);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 88);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "→";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 437);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "←";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 36;
+            this.listBox2.Location = new System.Drawing.Point(564, 270);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(170, 292);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(295, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(536, 117);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "農夫在森林採了一籃菜，牽著一隻羊，補了一隻狼\r\n但是他遇到了一條溪.. 農夫看著手邊3樣身家，苦惱了起來..\r\n\r\n請按 這裡 看看怎麼幫它！";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(867, 610);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project_OverRiver/OverRiver1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverRiver1
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _leftList;
+        private List<string> _rightList;
+        public Form1()
+        {
+            InitializeComponent();
+            原始清單();
+            清單選擇方式();
+            ChangeData();
+        }
+
+
+        private void 原始清單()
+        {
+            _leftList = new List<string> { "無辜的農夫", "看起來很餓的狼", "看起來很餓的羊", "瑟瑟發抖的高麗菜" };
+            _rightList = new List<string>();
+        }
+
+        private void 清單選擇方式()
+        {
+            listBox1.SelectionMode = SelectionMode.One; //多選
+            listBox2.SelectionMode = SelectionMode.One; //多選
+        }
+
+        private void ChangeData()
+        {
+            listBox1.DataSource = null;
+            listBox2.DataSource = null;
+            listBox1.DataSource = _leftList;
+            listBox2.DataSource = _rightList;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "1.只有農夫能夠划船，而且船比較小，除農夫之外每次只能運一種東西。\n\n" +
+                "2.還有一個棘手問題，就是如果沒有農夫看著，羊會偷吃菜，狼會吃羊。\n\n" +
+                "請考慮一種方法，讓農夫能夠安全地安排這些東西和他自己過河。";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string item = (string)listBox1.SelectedItem;
+                if (item == "無辜的農夫")
+                {
+                    _leftList.Remove(item);
+                    _rightList.Add(item);
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    _leftList.Remove(item);
+                    _rightList.Add(item);
+                    _leftList.Remove("無辜的農夫");
+                    _rightList.Add("無辜的農夫");
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+
+                ChangeData();
+
+                if (_leftList.Contains("瑟瑟發抖的高麗菜") && _leftList.Contains("看起來很餓的羊") && _leftList.Contains("看起來很餓的狼"))
+                {
+                    MessageBox.Show("你的身家被吃掉了啦！笨農夫！");
+                    Application.Restart();
+                }
+                else if (_leftList.Contains("瑟瑟發抖的高麗菜") && _leftList.Contains("看起來很餓的羊"))
+                {
+                    MessageBox.Show("很餓的羊把瑟瑟發抖的高麗菜\n ...吃掉了O口Q！快點重來！");
+                    Application.Restart();
+                }
+                else if (_leftList.Contains("看起來很餓的狼") && _leftList.Contains("看起來很餓的羊"))
+                {
+                    MessageBox.Show("很餓的狼嘿嘿嘿地接近了肥美的羊(in狼視角)...\n ... \n 羊羊被吃掉了！O口Q 快點重來！");
+                    Application.Restart();
+                }
+
+                else if (_rightList.Contains("瑟瑟發抖的高麗菜") && _rightList.Contains("看起來很餓的羊") &&
+                         _rightList.Contains("看起來很餓的狼") && _rightList.Contains("無辜的農夫"))
+                {
+                    MessageBox.Show("過河成功！所有的身家都平安無事！\n 大吉大利！今晚吃雞！");
+                    Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                string item = (string)listBox2.SelectedItem;
+                if (item == "無辜的農夫")
+                {
+                    _rightList.Remove(item);
+                    _leftList.Add(item);
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    _rightList.Remove(item);
+                    _leftList.Add(item);
+                    _rightList.Remove("無辜的農夫");
+                    _leftList.Add("無辜的農夫");
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                }
+
+                ChangeData();
+
+                if (_rightList.Contains("瑟瑟發抖的高麗菜") && _rightList.Contains("看起來很餓的羊") &&
+                          _rightList.Contains("看起來很餓的狼"))
+                {
+                    MessageBox.Show("你的身家被吃掉了！趕快重來！笨農夫！");
+                    Application.Restart();
+                }
+
+                else if (_rightList.Contains("瑟瑟發抖的高麗菜") && _rightList.Contains("看起來很餓的羊"))
+                {
+                    MessageBox.Show("很餓的羊把瑟瑟發抖的高麗菜\n ...吃掉了O口Q！快點重來！");
+                    Application.Restart();
+                }
+
+                else if (_rightList.Contains("看起來很餓的狼") && _rightList.Contains("看起來很餓的羊"))
+                {
+                    MessageBox.Show("很餓的狼嘿嘿嘿地接近了肥美的羊(in狼視角)...\n 羊羊被吃掉了！O口Q 快點重來！");
+                    Application.Restart();
+                }
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
