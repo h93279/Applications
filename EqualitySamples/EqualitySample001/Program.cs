cc7a24619dcf73080d228e9927c32caa844347d7
Nana <37291738+h93279@users.noreply.github.com>
2018-05-29 12:42:47 +0800

    Add files via upload

---
+++ EqualitySamples/EqualitySample001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualitySample001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 10;
+            Console.WriteLine("object.Equals(i,j) is " + object.Equals(i, j));   // true
+            
+            MyRectangle r1 = new MyRectangle { Width = 5, Height = 5 };
+            MyRectangle r2 = new MyRectangle { Width = 5, Height = 5 };
+            MyRectangle r3 = r2;
+
+            //Equals預設是比位置
+            Console.WriteLine("object.Equals(r1,r2) is " + object.Equals(r1, r2));   //false   <<比Heap的位子
+            Console.WriteLine("object.Equals(r2,r3) is " + object.Equals(r2, r3));   //true
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EqualitySamples/EqualitySample003/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualitySample003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //這個 Equals 是執行個體方法，表示自己與傳入的物件比較
+
+            int i = 10, j = 10;
+            Console.WriteLine("i.Equals(j) is " + i.Equals(j));   //true
+            // int 的  Equals 有多載，此處呼叫的是  int.Equals(int) 而非 object.Equals(object)
+
+            // ▲ 如果目前的執行個體是實值類型，此方法測試 值 是否相等。
+            // 1 兩個物件都屬於相同的型別。 
+            // 2 兩個物件的公用和私用欄位的值相等。
+
+            MyRectangle r1 = new MyRectangle { Width = 5, Height = 5 };
+            MyRectangle r2 = new MyRectangle { Width = 5, Height = 5 };
+            MyRectangle r3 = r2;
+
+            Console.WriteLine("r1.Equals(r2) is " + r1.Equals(r2));   //false
+            Console.WriteLine("r2.Equals(r3) is " + r2.Equals(r3));   //true
+            // ▲  如果目前的執行個體是參考類型，此方法測試 參考(位置) 是否相等，
+            //    而呼叫此方法相當於呼叫ReferenceEquals方法。
+            
+            Console.ReadLine();
+        }
+    }
+}
