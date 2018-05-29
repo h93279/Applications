17c4021c82032b27d8d861c3c5d2efd39332a0b6
Nana <37291738+h93279@users.noreply.github.com>
2018-05-29 12:33:59 +0800

    Add files via upload

---
+++ AddSample/AddSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 2;
+            int i2 = 3;
+            double d = 9.98;
+            string s1 = "one";
+            string s2 = "two";             // 注意：以下是糖
+            Console.WriteLine(i1 + i2);    // 23(X)     5(O)
+            Console.WriteLine(i1 + d);     // 29.98(X)  11.98(O)
+            Console.WriteLine(s1 + s2);    // onetwo
+            Console.WriteLine(s1 + i1);    // one2
+            Console.WriteLine(d + s2);     // 9.98two
+            Console.ReadLine();            // 有沒有覺得糖裡有毒:)
+
+        }
+    }
+}
