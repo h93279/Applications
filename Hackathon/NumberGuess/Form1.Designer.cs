142182f98fc885f62c4113bb6adeddc0d486795b
Nana <37291738+h93279@users.noreply.github.com>
2018-05-29 12:11:31 +0800

    Add files via upload

---
+++ Hackathon/NumberGuess/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace NumberGuess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(24, 208);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 146);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "\"雙擊\"即可開始遊戲";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.BurlyWood;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(23, 371);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(255, 73);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "我放棄！看答案！";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(307, 47);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 45);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(390, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "↓ 輸入你認為的四位數字↓";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(19, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 165);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "[ 遊戲規則 ]\r\n電腦會隨機產生一組4個數字\r\n請試著猜出那4個數字為何\r\n\r\n提示\r\nA表示數字正確、位置正確\r\nB表示數字正確、位置不正確\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Enabled = false;
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label3.Location = new System.Drawing.Point(307, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(388, 271);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "歷史紀錄\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(307, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(388, 58);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "檢查答案";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 515);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Hackathon/NumberGuess/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuess
+{
+    public partial class Form1 : Form
+    {
+
+        
+        int[] answers;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)  //開始
+        {
+            button1.Click += ChangeTheWords;
+        }
+
+        private void ChangeTheWords(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            button1.Text = "遊戲已開始！";
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            textBox1.Enabled = true;
+            textBox1.MaxLength = 4;
+            Random num = new Random();
+            answers = new int[4];
+            int i = 0;
+            while(i<4)
+            {
+                
+                int a = num.Next(0, 10);   //產生亂數0-9
+                /* foreach (var item in answers)
+                {
+                    if(!answers.Contains(a))
+                    {
+                        answers[i] = a;
+                    }
+                }*/
+                answers[i] = a;
+                for (int j = 0; j < i; j++) //推進answers判斷的位置
+                {
+                    if (answers[j] == a)
+                    {
+                        i--;
+                    }
+                }
+                i++;
+            }
+            //  MessageBox.Show(answers[0].ToString()+ answers[1].ToString() + answers[2].ToString() + answers[3].ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)  //放棄
+        {
+            MessageBox.Show("好吧真沒有耐心~答案是"+answers[0].ToString() + answers[1].ToString() + answers[2].ToString() + answers[3].ToString() + "啦!");
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            textBox1.Enabled = false;
+            button1.Text = "點擊再次開始！";
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //輸入欄
+        {
+
+        }
+        
+           
+        private void button3_Click(object sender, EventArgs e)   //確認鍵
+        {
+            char[] input = textBox1.Text.ToCharArray();
+            //MessageBox.Show("你輸入了"+ input[0].ToString() + input[1].ToString() + input[2].ToString() + input[3].ToString());
+
+            int a = 0;
+            int b = 0;
+            
+            if (textBox1.Text.Length == 4)
+            {
+                //var ins = answers.ToString().Intersect(input.ToString());
+                for (int i = 0; i < 4; i++)
+                {
+                    if (answers[i].ToString() == input[i].ToString())
+                    {
+                        a++;
+                        if (a == 4)
+                        {
+                            MessageBox.Show("恭喜你！答對囉！答案就是" + answers[0].ToString() + answers[1].ToString() + answers[2].ToString() + answers[3].ToString() + "啦!");
+                            button1.Enabled = true;
+                            button2.Enabled = false;
+                            textBox1.Enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        for( int j =0; j < 4; j++)
+                        {
+                            if (i == j)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                if(answers[i].ToString() == input[j].ToString())
+                                {
+                                    b++;
+                                }
+                            }
+                        }
+                        
+                    }
+
+                }
+                label3.Text += input[0].ToString()+ input[1].ToString()+ input[2].ToString() +input[3].ToString()+"----"+a + "A" + b + "B \n";
+            }
+            else  //如果為非4數字
+            {
+                MessageBox.Show("請輸入4個數字！");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)  //顯示欄
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
