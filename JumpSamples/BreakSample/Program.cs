7665e75dcb9f2b92e289049fc486756e23acdeaa
Nana <37291738+h93279@users.noreply.github.com>
2018-05-29 12:49:10 +0800

    Add files via upload

---
+++ JumpSamples/BreakSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("i = " + i);
+                for (int j = 11; j < 20; j++)
+                {
+                    Console.WriteLine("j = " + j);         //結果會變得不一樣
+                    if (j==15)
+                    {
+                        break;     //跳離最接近的外層(封閉式)迴圈
+                    }
+                    // Console.WriteLine("j = " + j);      //假如這句放在上面↑
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JumpSamples/ContinueSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinueSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("i = " + i);
+                for (int j = 11; j < 20; j++)
+                {
+                    if (j == 15)
+                    {
+                        continue;     //當符合時，忽略符合條件項目，繼續迴圈直至完成
+                    }
+                    Console.WriteLine("j = " + j);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
